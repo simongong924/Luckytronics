85194d4fb88a291ba748fbcdfcaf1cbb658200be
teoYQ <43258778+teoYQ@users.noreply.github.com>
2019-02-22 16:35:26 +0800

    Add files via upload

---
+++ src/UploadScreen.js
@@ -0,0 +1,14 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import React, { Component } from 'react';
+import axios from 'axios';
+import UploadScreen from './UploadScreen';
+class Login extends Component {
+constructor(props){
+  super(props);
+  this.state={
+  "you have logged in"
+  }
+ }}
