e4fb9552e90e37f04bdc9965f59bd3fee724b322
Simon Gong <simongong924@gmail.com>
2019-03-22 13:39:14 +0800

    backend user api

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const todoRoutes = express.Router();
-const PORT = 4000;
+const userRoutes = express.Router();
+const PORT = 5000;
 
-let Todo = require('./todo.model');
+let User = require('./user.model');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,46 +21,46 @@ connection.once('open', function() {
 })
 
 
-todoRoutes.route('/').get(function(req, res) {
-    Todo.find(function(err, todos) {
+userRoutes.route('/').get(function(req, res) {
+    User.find(function(err, users) {
         if (err) {
             console.log(err);
         } else {
-            res.json(todos);
+            res.json(users);
         }
     });
 });
 
-todoRoutes.route('/:id').get(function(req, res) {
+userRoutes.route('/:id').get(function(req, res) {
     let id = req.params.id;
-    Todo.findById(id, function(err, todo) {
-        res.json(todo);
+    User.findById(id, function(err, user) {
+        res.json(user);
     });
 });
 
-todoRoutes.route('/add').post(function(req, res) {
-    let todo = new Todo(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200).json({'todo': 'todo added successfully'});
+userRoutes.route('/add').post(function(req, res) {
+    let user = new User(req.body);
+    user.save()
+        .then(user => {
+            res.status(200).json({'user': 'user added successfully'});
         })
         .catch(err => {
-            res.status(400).send('adding new todo failed');
+            res.status(400).send('adding new user failed');
         });
 });
 
-todoRoutes.route('/update/:id').post(function(req, res) {
-    Todo.findById(req.params.id, function(err, todo) {
-        if (!todo)
+userRoutes.route('/update/:id').post(function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if (!user)
             res.status(404).send('data is not found');
         else
-            todo.todo_description = req.body.todo_description;
-            todo.todo_responsible = req.body.todo_responsible;
-            todo.todo_priority = req.body.todo_priority;
-            todo.todo_completed = req.body.todo_completed;
+            user.user_description = req.body.user_description;
+            user.user_responsible = req.body.user_responsible;
+            user.user_priority = req.body.user_priority;
+            user.user_completed = req.body.user_completed;
 
-            todo.save().then(todo => {
-                res.json('Todo updated');
+            user.save().then(user => {
+                res.json('User updated');
             })
             .catch(err => {
                 res.status(400).send("Update not possible");
@@ -68,7 +68,7 @@ todoRoutes.route('/update/:id').post(function(req, res) {
     });
 });
 
-app.use('/todos', todoRoutes);
+app.use('/users', userRoutes);
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
